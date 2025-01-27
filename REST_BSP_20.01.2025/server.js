810bab8f0a1d875b2fbb29cbbfddbb51df0d130d
) <100924402+DoubleUUUUU@users.noreply.github.com>
2025-01-27 12:24:20 +0100

    27.01.2025 Einheit 1

--- REST_BSP_20.01.2025/server.js
+++ REST_BSP_20.01.2025/server.js
@@ -4,9 +4,16 @@ const express = require("express")
 const app = express();
 const port = 3000;
 
+const data  = [
+   {name: "Hanna", age: 19},
+   {name: "Michael", age: 20},
+   {name: "Zoey", age: 19},
+   {name: "Kimi", age: 43},
+]
 
-app.get("/",(req, res)=>{
-    
+
+app.get("/people",(req, res)=>{
+    res.send(data);
 })
 
 app.listen(port,()=>{
